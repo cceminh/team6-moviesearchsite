956278ef8fe37889896a9f02a5e53bf37185ff46
Choe <ceminh97@naver.com>
2023-10-29 16:40:26 +0900

    s

--- modules/feat_js/api.js
+++ modules/feat_js/api.js
@@ -5,8 +5,10 @@ const a = await fetchMovieData("upcoming");
 const b = await fetchMovieData("top_rated");
 const c = await fetchMovieData("popular");
 
+// 중복 제거 전 배열 {60}
 export const combinedArray = [...a, ...b, ...c];
 
+//중복 제거 배열 {57}
 export const unique = [
   ...new Map(combinedArray.map((m) => [m.id, m])).values(),
 ];
