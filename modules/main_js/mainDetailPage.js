216416e96846d5c7eea6a1afafbbf0518a767d9b
junny092597 <junny092597@naver.com>
2023-10-30 12:49:33 +0900

    리뷰 기능 추가

--- modules/main_js/mainDetailPage.js
+++ modules/main_js/mainDetailPage.js
@@ -2,9 +2,10 @@
 import { generateMovieDetailCards } from "../feat_js/clickedMovieShow.js";
 import { goHomeBtn } from "../feat_js/goHome.js";
 import { goBackBtn } from "../feat_js/goHome.js";
+import {initializeCommentSystem} from "../review_js/commentsystem.js"
 /* 상세정보 영화카드 만들기 */
 generateMovieDetailCards();
-
+initializeCommentSystem()
 
 /* 홈으로 가기 go*/
 goHomeBtn.addEventListener('click', (event) => {
---
+++ modules/review_js/commentsystem.js
@@ -0,0 +1,106 @@
+export function initializeCommentSystem() {
+    const commentForm = document.getElementById('commentForm');
+    const commentList = document.getElementById('commentList');
+    let editIndex = -1;
+
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const pageId = urlParams.get('id');
+
+    
+    const pageKey = 'comments_' + pageId;
+
+    function loadCommentsFromLocalStorage() {
+        const comments = JSON.parse(localStorage.getItem(pageKey)) || [];
+        comments.forEach(comment => {
+            addCommentToUI(comment);
+        });
+    }
+
+    function addCommentToUI(comment) {
+        const commentElement = document.createElement('li');
+        commentElement.innerHTML = `
+            <strong>${comment.username}</strong>: ${comment.commentText}
+            <button data-action="edit">Edit</button>
+            <button data-action="delete">Delete</button>
+        `;
+
+        commentList.appendChild(commentElement);
+    }
+
+    function editComment(event) {
+        const listItem = event.target.parentElement;
+        editIndex = Array.from(listItem.parentNode.children).indexOf(listItem);
+        const commentElement = listItem.querySelector('strong');
+        const commentText = commentElement.textContent.split(': ')[1];
+        document.getElementById('username').value = commentElement.textContent.trim();
+        document.getElementById('comment').value = commentText;
+    }
+
+    function editCommentInUI(comment) {
+        const listItem = commentList.children[editIndex];
+        const commentElement = listItem.querySelector('strong');
+        commentElement.innerHTML = comment.username;
+        listItem.innerHTML = `
+            <strong>${comment.username}</strong>: ${comment.commentText}
+            <button data-action="edit">Edit</button>
+            <button data-action="delete">Delete</button>
+        `;
+    }
+
+    function deleteComment(event) {
+        const listItem = event.target.parentElement;
+        const index = Array.from(listItem.parentNode.children).indexOf(listItem);
+        commentList.removeChild(listItem);
+
+        const comments = getCommentsFromUI();
+        comments.splice(index, 1);
+        saveCommentsToLocalStorage(comments);
+    }
+
+    function getCommentsFromUI() {
+        const comments = [];
+        commentList.querySelectorAll('li').forEach(commentElement => {
+            const username = commentElement.querySelector('strong').textContent;
+            const commentText = commentElement.textContent.split(': ')[1];
+            comments.push({ username, commentText });
+        });
+        return comments;
+    }
+
+    function saveCommentsToLocalStorage(comments) {
+        comments = comments || getCommentsFromUI();
+        localStorage.setItem(pageKey, JSON.stringify(comments));
+    }
+
+    commentForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const commentText = document.getElementById('comment').value;
+
+        const comment = { username, commentText };
+
+        if (editIndex === -1) {
+            addCommentToUI(comment);
+        } else {
+            editCommentInUI(comment);
+            editIndex = -1;
+        }
+
+        saveCommentsToLocalStorage();
+
+        document.getElementById('username').value = '';
+        document.getElementById('comment').value = '';
+    });
+
+    document.addEventListener('click', (event) => {
+        if (event.target.dataset.action === 'edit') {
+            editComment(event);
+        } else if (event.target.dataset.action === 'delete') {
+            deleteComment(event);
+        }
+    });
+
+    loadCommentsFromLocalStorage();
+}
